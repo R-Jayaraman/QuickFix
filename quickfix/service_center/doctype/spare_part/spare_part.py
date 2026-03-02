10675ec8d3d27ff060237fb30d8d2244848b670a
jayaraman <95jayaraman.r63@gmail.com>
2026-03-02 15:15:28 +0530

    feat: doctype creation and update device type

---
+++ quickfix/service_center/doctype/spare_part/spare_part.py
@@ -0,0 +1,18 @@
+import frappe
+from frappe.model.document import Document
+
+class SparePart(Document):
+
+    def autoname(self):
+        if not self.part_code:
+            frappe.throw("Part Code is required")
+
+        code = self.part_code.upper()
+
+        series = frappe.model.naming.make_autoname("PART-.YYYY.-.####")
+
+        self.name = f"{code}-{series}"
+
+    def validate(self):
+        if self.selling_price <= self.unit_cost:
+            frappe.throw("Selling Price must be greater than Unit Cost")
